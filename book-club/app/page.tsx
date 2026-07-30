138353cb4a1c75282decf4d7262c7af418f1957b
49djdjd <azra_ali09@outlook.com>
2026-07-30 10:32:31 -0400

    working on evelope but still looks weird (added gradient and shadow)

--- book-club/app/page.tsx
+++ book-club/app/page.tsx
@@ -6,11 +6,13 @@ export default function Home() {
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex justify-center items-center">
           <div className="group">
-            <div className=" flex relative justify-center w-2xl h-125 bg-taupe-200">
+            <div className=" flex relative justify-center w-2xl h-125  bg-sky-900">
+              <div className="drop-shadow-md w-full h-80">
               {/* mozilla/mdn is used to reference how to do clip path */}
-              <div className="bg-yellow-100 w-full h-80 [clip-path:polygon(0_0,100%_0,50%_100%)]">
+              <div className="bg-sky-900 opacity-70 w-full h-80 bg-gradient-to-b from-sky-800 to-sky-900 [clip-path:polygon(0_0,100%_0,50%_100%)]">
               </div>
-              <div className="absolute flex items-center z-0 justify-center rounded-full opacity-75 bg-cyan-700 w-20 h-20 font-stamp translate-y-9/3 ">
+              </div>
+              <div className="absolute flex items-center z-0 justify-center rounded-full opacity-85 bg-cyan-700 w-20 h-20 font-stamp translate-y-9/3 ">
                 <h1 className="text-6xl z-40 text opacity-100 -cyan-600">A </h1>
               </div>
               
