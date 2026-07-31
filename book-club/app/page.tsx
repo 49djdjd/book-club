ed343261c6996c73a875cc4ebef31e83d68c724e
49djdjd <azra_ali09@outlook.com>
2026-07-31 12:05:37 -0400

    working on making the envelope flip with the button (restructuring the divs...)

--- book-club/app/page.tsx
+++ book-club/app/page.tsx
@@ -4,27 +4,38 @@ export default function Home() {
   const [username, setUserName] = useState("");
   const [message, setMessage] = useState("");
   const [submit, setSubmit] = useState(false);
-  const [envOpen, isEnvOpen] = useState("false")
+  const [envelope, openEnvelope] = useState(false)
+  const [envOpen, isEnvOpen] = useState(false)
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-yellow-50 font-sans">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16">
         <div className="flex justify-center items-center">
           <div className="group relative">
-            <div className=" flex relative justify-center w-2xl h-125 shadow-md bg-sky-900">
+            <div className=" flex relative justify-center mt-50 w-2xl h-125 shadow-md bg-sky-900">
               {/* used ai for debugging drop shadow (issue with the clip-path) */}
               <div className="drop-shadow-md w-full h-80">
+                <div>
               {/* mozilla/mdn is used to reference how to do clip path, ai was used to debug to make the gradient bottom only */}
-              <div className="group bg-sky-900 opacity-70 w-full h-80 bg-gradient-to-b from-sky-800 to-sky-900 origin-top transition-transform duration-500 rotate-x-0 group-hover:-rotate-x-180 [clip-path:polygon(0_0,100%_0,50%_100%)]">
+              <div className={`bg-sky-900 opacity-70 w-full h-80 bg-gradient-to-b from-sky-800 to-sky-900 origin-top transition-transform duration-500 [clip-path:polygon(0_0,100%_0,50%_100%)] ${envelope ? `-scale-y-100 opacity-100` : `scale-y-100`}`}>
               </div>
-              </div>
-              <div className="absolute flex items-center z-0 justify-center rounded-full opacity-85 bg-cyan-700 w-20 h-20  translate-y-9/3 hover: ">
-                <button onClick={() => setMessage("STOP! Do not read! This mail is reserved for a special person... By  the way, what is your name? ")} className="text-5xl text-blue-100">
+              <div className={`absolute flex items-center z-0 justify-center rounded-full opacity-85 bg-cyan-700 w-20 h-20  translate-y-9/3 transition-transform duration-500 ${envelope ? '-translate-y-20' : 'rotate-0'}`}>
+                <button onClick={() => setMessage("STOP! Do not read! This mail is reserved for a special person... By  the way, what is your name? ")} className="text-5xl text-blue-100 ">
                   <h1 className="text-6xl z-40 font-stamp text opacity-100 -cyan-600">A</h1>
                   </button>
               </div>
+              </div>
+              {/* referenced ai for the syntax of swithching tailwind based on states */}
+              <div className={`bg-white -translate-y-2/3 text-black font-mono translate-x-1/12 w-xl transition-transform duration-500 h-100 ${envelope ? `opacity-100 -translate-y-[90%]` : `opacity-0 -translate-y-2/3` }`}>
+              <h1>Dear {username}, </h1>
+              <br></br>
+              <p>(not sure what to do..)You are invited to our exclusive book club! We are going to be having a meeting later today, please come ready with the book fully read AND annotated so that we may have a proper discussion! This will determine your future standing within the club...</p>
+
+              </div>
+              </div>
+              
               {/* used ai to figure out what inset 0 was and how to use it (was not centering for the life of me...) also i did message on the outside to make sure it was clickable */}
               {message && (
-                <div className="absolute items-center inset-0 flex justify-center ">
+                <div className="absolute items-center inset-0 flex justify-center  ">
                   <div className="bg-white p-8 justify-center font-mono text-center flex w-xl items-center">
                     {/* referneced ai on how to work the handle submit (i didnt know how to make it based on enter (but i do now!)) */}
                     <p className="z-50 text-xl w-lg text-black">{message} <br></br> <form onSubmit={(e) => {
@@ -37,7 +48,7 @@ export default function Home() {
                     {!submit ? (
                     <input type="text" value={username} onChange={(e) => setUserName(e.target.value)} className="bg-stone-100 mt-2 hover:bg-stone-200"></input> 
                     ):(
-                      <button onClick={()=> setMessage("")} className="text-sm rounded-sm bg-stone-100 mt-4 p-2 text-gray">Close</button>
+                      <button onClick={()=> { setMessage(""); openEnvelope(true); }} className="text-sm rounded-sm bg-stone-100 mt-4 p-2 text-gray">Close</button>
                     )}
                     </form> </p>
                     {/* used ai to double check for the syntax of the on change */}
