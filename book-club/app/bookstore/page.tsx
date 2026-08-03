2d6b3965e5fb4ebab4f52ae0b70b89035d13c0e3
49djdjd <azra_ali09@outlook.com>
2026-08-03 12:36:10 -0400

    positioned the books and starting to build the function for the first mini-game.

--- book-club/app/bookstore/page.tsx
+++ book-club/app/bookstore/page.tsx
@@ -1,13 +1,46 @@
+import Image from 'next/image';
+import { useState } from 'react';
+const [game, startGame] = useState(false);
+const [cardFlip, setCardsFlip] = useState(false)
 export default function Home(){
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-yellow-50 font-sans">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16">
-        <div className="flex justify-center items-center">
-          <div className="group relative">
+    <div className="flex flex-1 items-center justify-center bg-yellow-50 font-sans">
+      <main className="flex flex-1 flex-col w-full max-w-3xl  items-center justify-between py-32 px-16">
+        <div className="flex flex-col justify-center items-center">
             <h3 className="text-black font-mono text-xl">Welcome! Which book would you like to purchase? (Remember you can only pick one these books!)</h3>
             </div>
-               
+      {/* used ai to learn abuot over-flow hidden and refenrced for the div structure (didnt know how to make all of these images the same size) */}
+            <div className="flex items-center justify-center mt-4  gap-4">
+          <div className="w-48 h-64 overflow-hidden">
+            <Image
+              src="/stonerBook.jpg"
+              alt="Stoner Cover"
+              width={200}
+              height={300}
+              className="w-full h-auto"
+            />
           </div>
+          <div className="w-48 h-64 overflow-hidden">
+            <Image onClick={()=> startGame(true)}
+              src="/theBellJar.jpg"
+              alt="The Bell Jar Cover"
+              width={200}
+              height={300}
+              className="w-full h-auto"
+            />
+          </div>
+          <div className="w-48 h-64 overflow-hidden">
+            <Image
+              src="/theTalentedMrRipley.jpg"
+              alt="The Talented Mr. Ripley Cover"
+              width={200}
+              height={300}
+              className="w-full h-auto"
+            />
+          </div>
+        </div>
+       
+          
         {}
       </main>
     </div>
