269bee5e2dc5c1f748adf330f52fd12c294e9a1e
49djdjd <azra_ali09@outlook.com>
2026-08-05 09:23:06 -0400

    hmm the cards are moving around but its really glitchy and they arent being flipped...

--- book-club/app/bookstore/page.tsx
+++ book-club/app/bookstore/page.tsx
@@ -2,9 +2,9 @@
 import Image from 'next/image';
 import { useState } from 'react';
 const Icards = [
-  {id: 1, bookId: 'stoner', title: 'Stoner', imgSrc:'/1stonerBook.jpg', isFlipped: false, isMatched: false},
-  {id: 2, bookId: 'bell-jar', title: 'The Bell Jar', imgSrc:'/theBellJar.jpg', isFlipped: false, isMatched: false},
-  {id: 3, bookId: 'ripley', title: 'The Talented Mr. Ripley', imgSrc:'/theTalentedMrRipley.jpg', isFlipped: false, isMatched: false},
+  {id: 1, bookId: 'stoner', title: 'Stoner', imgSrc:'/1stonerBook.jpg', isFlipped: true, isMatched: false},
+  {id: 2, bookId: 'bell-jar', title: 'The Bell Jar', imgSrc:'/theBellJar.jpg', isFlipped: true, isMatched: false},
+  {id: 3, bookId: 'ripley', title: 'The Talented Mr. Ripley', imgSrc:'/theTalentedMrRipley.jpg', isFlipped: true, isMatched: false},
 ];
 
 function mixArray<T>(array:T[]) : T[]{
@@ -22,8 +22,20 @@ export default function Home(){
       setCards(mixArray(cards).map(card => ({...card, isFlipped:true})));
       startGame(true);
     }
+    return;
   }
-  }
+    // this is because react has immutable states
+      setCards(prevCards => {
+        return prevCards.map(card => {
+          if(card.id == clickedCard.id){
+            return{...card, isFlipped: true}
+          }
+          else{
+            return card;
+          }
+          });
+        });
+      }
   return (
     <div className="flex flex-1 items-center justify-center bg-yellow-50 font-sans">
       <main className="flex flex-1 flex-col w-full max-w-3xl  items-center justify-between py-32 px-16">
@@ -34,7 +46,8 @@ export default function Home(){
           <div className="flex items-center [perspective:1000px] justify-center mt-10 gap-4">
           {/* learned how to use the map function to go through arrays and what key was*/}
           {cards.map((card) => (
-            <div key={card.id} className='relative w-48 h-64'>
+            <div key={card.id} onClick={() => clickCard(card)} className='relative w-48 h-64'>
+            {card.isFlipped ? (
               <Image
               src={card.imgSrc}
               alt={card.title}
@@ -42,6 +55,10 @@ export default function Home(){
               height={300}
               className='w-full h-auto'
               />
+
+            ) : (
+           <div className='w-full h-auto bg-black'></div>
+            )}
             </div>
           ))}
         </div>
